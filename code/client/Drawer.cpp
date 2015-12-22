0c1ddf0260745a590318945265b6f6858a5c489f
kassHH <kacha.herman@gmail.com>
2015-12-22 19:16:49 +0100

    testowy commit na nowym branch'u

--- code/client/Drawer.cpp
+++ code/client/Drawer.cpp
@@ -15,5 +15,6 @@ void Drawer::draw(QPainter *painter)
     qDebug() << "paint event, phase overlay = " << phaseOverlay;
     painter->drawLine(10, 10, 40, 40);
     painter->drawPixmap(70, 70, BOARD_FIELD_WIDTH, BOARD_FIELD_HEIGHT, sprites.get(BOARD_FIELD_ID(FLOWERGRASS)));
+painter->drawPixmap(140, 140, BOARD_FIELD_WIDTH, BOARD_FIELD_HEIGHT, sprites.get(BOARD_FIELD_ID(FLOWERGRASS)));
     painter->drawPixmap(70, 70, BOARD_FIELD_WIDTH, BOARD_FIELD_HEIGHT, sprites.get(BOARD_FIELD_ID(TANK)));
 }
