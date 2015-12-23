822c6e4d2681471075484fa410c3134e0baab3e1
kassHH <kacha.herman@gmail.com>
2015-12-23 22:48:59 +0100

    ogólna wizja i problemy - plik wzory.txt

--- code/client/Drawer.cpp
+++ code/client/Drawer.cpp
@@ -7,14 +7,15 @@ Drawer::Drawer(Game *game)
     :game(game), sprites()
 {
 }
-
 void Drawer::draw(QPainter *painter)
 {
     float phaseOverlay = game->getPhaseOverlay();
     game->model1.board[2][3];
     qDebug() << "paint event, phase overlay = " << phaseOverlay;
     painter->drawLine(10, 10, 40, 40);
+    QTransform transf;
     painter->drawPixmap(70, 70, BOARD_FIELD_WIDTH, BOARD_FIELD_HEIGHT, sprites.get(BOARD_FIELD_ID(FLOWERGRASS)));
-painter->drawPixmap(140, 140, BOARD_FIELD_WIDTH, BOARD_FIELD_HEIGHT, sprites.get(BOARD_FIELD_ID(FLOWERGRASS)));
-    painter->drawPixmap(70, 70, BOARD_FIELD_WIDTH, BOARD_FIELD_HEIGHT, sprites.get(BOARD_FIELD_ID(TANK)));
+    painter->drawPixmap(70, 140, BOARD_FIELD_WIDTH, BOARD_FIELD_HEIGHT, sprites.get(BOARD_FIELD_ID(GRASS)));
+    painter->drawPixmap(70, 70, BOARD_FIELD_WIDTH, BOARD_FIELD_HEIGHT, sprites.get(BOARD_FIELD_ID(TANK)).transformed(transf.rotate(45)));
+painter->drawPixmap(70, 140, BOARD_FIELD_WIDTH, BOARD_FIELD_HEIGHT, sprites.get(BOARD_FIELD_ID(TANK)));
 }
