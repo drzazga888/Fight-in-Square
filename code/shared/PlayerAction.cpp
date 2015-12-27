880bf4edc2ea4d63787123aedecfcf1d4451546b
Kamil Drzazga <drzazga888@gmail.com>
2015-12-27 11:10:40 +0100

    Dokonczenie wielkich zmian, dodanie dokumentacji

---
+++ code/shared/PlayerAction.cpp
@@ -0,0 +1,5 @@
+#include "PlayerAction.h"
+
+PlayerAction::PlayerAction()
+{
+}
---
+++ code/shared/PlayerAction.h
@@ -0,0 +1,10 @@
+#ifndef PLAYERACTION_H
+#define PLAYERACTION_H
+
+class PlayerAction
+{
+public:
+    PlayerAction();
+};
+
+#endif // PLAYERACTION_H
